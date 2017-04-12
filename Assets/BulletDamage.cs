707612b07e095ae18e9b00bcf5976ef39e103140
osanchmo <laliloli1>
2017-04-12 18:40:15 +0200

    SpaceShooter
    Space shooter game with basic scenary

---
+++ Assets/BulletDamage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour {
+    	void OnCollisionEnter2D(Collision2D coll) {
+		if(coll.gameObject.tag == "enemyShip") {
+			Destroy(coll.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/spawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+	public GameObject ship;
+
+	public float interval = 2;
+	void Start() {
+		InvokeRepeating("SpawnNext",interval, interval);
+	}
+
+	void SpawnNext (){
+		Instantiate(ship, transform.position, Quaternion.identity);
+	}
+}
